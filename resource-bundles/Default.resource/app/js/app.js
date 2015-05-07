081fdfcb59fff3b37a33a9e7f344cec8f868453b
Scott Covert <scott.b.covert@gmail.com>
2015-05-07 17:28:31 -0400

    Added lodash and ngLoading for pretty loading

--- resource-bundles/Default.resource/app/js/app.js
+++ resource-bundles/Default.resource/app/js/app.js
@@ -1,13 +1,26 @@
 'use strict';
 
 
-var myModule = angular.module('Default', ['ngRoute','ngAnimate','Default.services','Default.filters','Default.directives','Default.controllers','ui.bootstrap']);
+var myModule = angular.module('Default', ['ngRoute','ngAnimate','Default.services','Default.filters','Default.directives','Default.controllers','ui.bootstrap','ngLoading']);
 
 // Declare app level module which depends on filters, and services
 myModule.
-  config(['$routeProvider', function($routeProvider) {
+  config(['$routeProvider', 'loadingProvider', function($routeProvider, loadingProvider) {
     $routeProvider
     	.when('/', {templateUrl: sitePrefix+'/DefaultHome_Accounts', controller: 'HomeCtrl'})
     	.when('/contacts', {templateUrl: sitePrefix+'/DefaultHome_Contacts', controller: 'ContactsCtrl'})
     	.otherwise({redirectTo: '/'});
+
+    //ngLoading Config
+    loadingProvider
+      .load({
+          transitionSpeed: '.3s', //default
+          class: 'spinner',
+          overlay: {
+            display: true, //required to apply an overlay
+            color: '#FEFEFE', //default
+            opacity: '.3' //default
+          }
+      });
+
   }]);
--- resource-bundles/Default.resource/app/js/controllers.js
+++ resource-bundles/Default.resource/app/js/controllers.js
@@ -3,19 +3,27 @@
 /* Controllers */
 
 angular.module('Default.controllers', [])
-  .controller('HomeCtrl', ['$scope', '$rootScope', '$filter', '$q', '$interval', '$location', 'Accounts', function($scope, $rootScope, $filter, $q, $interval, $location, Accounts) {
+  .controller('HomeCtrl', ['$scope', '$rootScope', '$filter', '$q', '$interval', '$location', '$timeout', 'Interceptor', 'Accounts', function($scope, $rootScope, $filter, $q, $interval, $location, $timeout, Interceptor, Accounts) {
+  	Interceptor.start();
   	Accounts.get()
   		.then(function(result){
   			$scope.accounts = result;
+  			$timeout(function(){
+  				Interceptor.end();
+  			},2000);
   		},
   		function(reason){
   			console.log('ERROR: ' + reason);
   		})
   }])
-  .controller('ContactsCtrl', ['$scope', '$rootScope', '$filter', '$q', '$interval', '$location', 'Contacts', function($scope, $rootScope, $filter, $q, $interval, $location, Contacts) {
+  .controller('ContactsCtrl', ['$scope', '$rootScope', '$filter', '$q', '$interval', '$location', '$timeout', 'Interceptor', 'Contacts', function($scope, $rootScope, $filter, $q, $interval, $location, $timeout, Interceptor, Contacts) {
+  	Interceptor.start();
   	Contacts.get()
   		.then(function(result){
   			$scope.contacts = result;
+  			$timeout(function(){
+  				Interceptor.end();
+  			},2000);
   		},
   		function(reason){
   			console.log('ERROR: ' + reason);
